efa63e31be2e8aec1983d678f361d8e5f7cd600d
Linggos06 <julia.snk@hotmail.com>
2021-11-05 15:50:13 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    speed: 950,
+ 
+    freeMode: {
+     enabled: true,
+     sticky: true,
+   },
+ 
+    autoplay: {
+       delay: 5000,
+     },
+    
+    breakpoints: {
+       // when window width is >= 320px
+       320: {
+         slidesPerView: 1.3,
+         spaceBetween: 40
+       },
+       // when window width is >= 605px
+       605: {
+         slidesPerView: 2.3,
+         spaceBetween: 20
+       },
+       // when window width is >= 993px
+       993: {
+         slidesPerView: 3,
+         spaceBetween: 30
+       }
+     },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+ 
+  
+  });
